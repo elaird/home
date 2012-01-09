f094e8d8ed5d0c63adf76779258cdd695a574cb7
Edward Laird <elaird@gmail.com>
2012-01-09 14:45:34 +0100

    float evo and comment volume attempts

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,18 @@
 import XMonad
 import XMonad.Layout.Spiral
 import XMonad.Util.EZConfig
+--import XMonad.Actions.Volume
 
 --mylayoutHook = spiral (6/7) ||| Mirror tiled ||| Full
 --               where tiled = Tall 1 0.03 0.5 
 
+-- determine classNames with xprop | grep WM_CLASS
 mymanageHook :: ManageHook
 mymanageHook = composeAll
-                [ className =? "Kruler" --> doFloat ]
-
+                [ className =? "Kruler" --> doFloat
+                , className =? "net-sourceforge-jnlp-runtime-Boot" --> doFloat
+                , stringProperty "WM_NAME" =? "Vievo" --> doFloat
+                ]
 
 main = xmonad $ defaultConfig
        { terminal = "konsole"
@@ -16,10 +20,12 @@ main = xmonad $ defaultConfig
        , manageHook = mymanageHook
        , borderWidth = 0
        , startupHook = do spawn ". ~/.xmodmap" 
---                       >> spawn "hsetroot -full ~/.background"
---                       >> spawn "xcompmgr &"
+                       >> spawn "xloadimage -onroot -center ~/.background"
        }
        `additionalKeys`  [ ((mod1Mask, xK_p), spawn "dmenu_run")
+--                         , ((modMask x, xK_F8 ), lowerVolume 3 >> return ()),
+--                         , ((modMask x, xK_F9 ), raiseVolume 3 >> return ()),
+--                         , ((modMask x, xK_F10), toggleMute    >> return ()),
        			 , ((mod1Mask, xK_z), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
 			 ]
        `additionalKeysP` [ ("M-C-l", spawn "slock") ]
