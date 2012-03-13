8c5ec4949ab3ad2f1be770baa7d6094952036ad2
Edward Laird <elaird@gmail.com>
2012-03-13 21:24:54 +0100

    prune

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,5 @@
 import XMonad
-import XMonad.Layout.Spiral
 import XMonad.Util.EZConfig
---import XMonad.Actions.Volume
-
---mylayoutHook = spiral (6/7) ||| Mirror tiled ||| Full
---               where tiled = Tall 1 0.03 0.5 
 
 -- determine classNames with xprop | grep WM_CLASS
 mymanageHook :: ManageHook
@@ -16,16 +11,10 @@ mymanageHook = composeAll
 
 main = xmonad $ defaultConfig
        { terminal = "konsole"
---       , layoutHook = mylayoutHook
        , manageHook = mymanageHook
        , borderWidth = 0
-       , startupHook = do spawn ". ~/.xmodmap" 
-                       >> spawn "xloadimage -onroot -center ~/.background"
        }
        `additionalKeys`  [ ((mod1Mask, xK_p), spawn "dmenu_run")
---                         , ((modMask x, xK_F8 ), lowerVolume 3 >> return ()),
---                         , ((modMask x, xK_F9 ), raiseVolume 3 >> return ()),
---                         , ((modMask x, xK_F10), toggleMute    >> return ()),
        			 , ((mod1Mask, xK_z), spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi") -- %! Restart xmonad
 			 ]
        `additionalKeysP` [ ("M-C-l", spawn "slock") ]
